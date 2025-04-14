e5b3ed338a3869607a87a6f8580932c68e5feeda
tizianotambone <tambonetiziano@gmail.com>
2025-04-14 14:25:06 +0200

    [UPD]ex3done

--- function.js
+++ function.js
@@ -13,3 +13,9 @@ function hobbies(hobbyOne, hobbyTwo, hobbyThree){
     return[hobbyOne, hobbyTwo, hobbyThree];
 }
 module.exports={hobbies};
+
+function cose(){
+    return{fullName:fullName("gino","paoli"), hobbies:hobbies("basket","calcio","tennis")}
+}
+
+module.exports={cose};
---
+++ people.js
@@ -0,0 +1,5 @@
+const nomeCognome=require(`./function.js`);
+const hobbies=require(`./function.js`);
+
+const mix= nomeCognome.cose()
+console.log(mix)
